fbf6de1951ec43380ff8609768f38c12eeca9410
Davy jones <hello@andrewwilkinson.me>
2025-05-13 09:21:16 +0100

    Add profile picture as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.jpeg" type="image/jpeg" />
+      </head>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
